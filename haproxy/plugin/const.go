064d8493b27916e1fa47ed46e65ee7a98d69e34f
John Calabrese <jcalabrese@pivotal.io>
2016-12-07 12:51:18 -0500

    [#135176031] fixes incorrect jobname

--- haproxy/plugin/const.go
+++ haproxy/plugin/const.go
@@ -8,7 +8,7 @@ const (
 	defaultStemcellAlias        = "trusty"
 	defaultStemcellVersion      = "3232.17"
 	DefaultInstanceGroupName    = "external-haproxy"
-	DefaultJobName              = "haproxy-job"
+	DefaultJobName              = "haproxy"
 	DefaultHaProxyInstanceCount = 1
 	DefaultReleaseURL           = "https://bosh.io/d/github.com/cloudfoundry-community/haproxy-boshrelease?v=8.0.9"
 	DefaultReleaseSHA           = "13598c70a50f8caf95d06782d67610daede8aeb9"
